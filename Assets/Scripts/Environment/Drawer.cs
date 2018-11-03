b0ba190541de5b6dcf4a39d2a46b1874d8524843
Andrew Hung <awhhung@gmail.com>
2018-11-03 09:58:39 -0700

    initial drawers

---
+++ Assets/Scripts/Environment/Drawer.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drawer : Interactable {
+
+    public Vector3 openExtendVector;
+    public float openCloseDuration;
+
+    bool isOpen;
+    Vector3 closedPosition;
+    Vector3 openedPosition;
+    Rigidbody rb;
+
+    private void Awake()
+    {
+        closedPosition = transform.position;
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        // put this in update so we can tweak it in inspector
+        openedPosition = closedPosition + openExtendVector;
+    }
+
+    public override void Interact(Pickupable heldItem)
+    {
+        base.Interact(heldItem);
+        isOpen = !isOpen;
+
+        if (isOpen)
+        {
+            rb.MovePosition(openedPosition);
+        }
+        else
+        {
+            rb.MovePosition(closedPosition);
+        }
+    }
+}
