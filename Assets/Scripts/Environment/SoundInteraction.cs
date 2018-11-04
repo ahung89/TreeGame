065b3e7c406516524524a7f8b1c8c530960c30b0
aphermus <aphermus@gmail.com>
2018-11-04 01:22:34 -0700

    Add locked door interaction

---
+++ Assets/Scripts/Environment/SoundInteraction.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Scripts.Environment
+{
+    class SoundInteraction : Interactable
+    {
+        AudioSource source;
+
+        void Awake()
+        {
+            source = gameObject.GetComponent<AudioSource>();
+        }
+
+        public override bool CanInteractWith(Pickupable heldItem)
+        {
+            return !heldItem;
+        }
+
+        public override bool Interact(Pickupable heldItem)
+        {
+            if (!source.isPlaying)
+            {
+                source.Play();
+            }
+            return false;
+        }
+    }
+}
