2f60d07258c5cadfd8b83cc119cb112e04b02661
Andrew Hung <awhhung@gmail.com>
2018-11-04 14:07:14 -0800

    add helper to kick off cinematic

---
+++ Assets/Scripts/CinematicController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Playables;
+using UnityEngine;
+
+public class CinematicController : MonoBehaviour {
+
+    public static CinematicController Instance;
+
+    public GameObject cinematicDirector;
+
+    private void Awake()
+    {
+        if (!Instance)
+        {
+            Instance = this;
+        }
+    }
+
+    void Update () {
+        // test
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            StartCinematic();
+        }
+	}
+
+    public void StartCinematic()
+    {
+        cinematicDirector.SetActive(true);
+    }
+}
