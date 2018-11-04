89a7226002f30260abe2916cfa987570fa28ab88
ackleyrc <ackleyrc@gmail.com>
2018-11-03 17:38:52 -0700

    Add ability to give the tree a glass of milk

---
+++ Assets/Scripts/Items/Tree.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tree : Interactable
+{
+    public override bool CanInteractWith(Pickupable heldItem)
+    {
+        return heldItem != null; // for now, let's highlight the tree any time you bring it an object
+    }
+
+    public override bool Interact(Pickupable heldItem)
+    {
+        /// In order, the tree needs:
+        ///     A Glass of Milk
+        ///     A Stuffed Animal
+        ///     Book Read Aloud
+        ///     Fire Put Out
+        ///     Flute Played
+        
+        if (heldItem)
+        {
+            Cup cup = heldItem.GetComponent<Cup>();
+            if (cup)
+            {
+                if (cup.currentLiquid == Cup.Liquid.Milk)
+                {
+                    SequenceTracker.Instance.milkConsumed = true;
+
+                    // elicit a positive reaction
+                    Debug.Log("Mmm, Milk is good for strong branches!");
+                    return false; // No need to try picking up or dropping after this interation
+                }
+            }
+        }
+
+        Debug.Log("Nope, don't want that");
+        return false; // No need to try picking up or dropping after this interation
+    }
+}
--- Assets/Scripts/SequenceTracker.cs
+++ Assets/Scripts/SequenceTracker.cs
@@ -11,14 +11,15 @@ public class SequenceTracker : MonoBehaviour
         if (!Instance)
         {
             Instance = this;
+
         }
     }
     // Singleton pattern 
 
     // These flags indicate whether actions in the desired sequence have been successfully completed
-    bool milkConsumed = false;
-    bool teddyBearProvided = false;
-    bool bookRead = false;
-    bool fireOut = false;
+    public bool milkConsumed = false;
+    public bool teddyBearProvided = false;
+    public bool bookRead = false;
+    public bool fireOut = false;
     // bool flutePlayed = false; // Will this be necessary? Maybe initiate a Coroutine on successfully playing the flute
 }
