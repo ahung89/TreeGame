d812159feadc1791feadb9cd4b8d594a0f2f4eb6
Andrew Hung <awhhung@gmail.com>
2018-11-04 17:04:18 -0800

    make animations better

---
+++ Assets/RowanAnim.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RowanAnim : MonoBehaviour {
+
+    Animator anim;
+
+	void Start () {
+        anim = GetComponent<Animator>();		
+	}
+	
+	public void Chill()
+    {
+        Debug.Log("chill");
+        anim.SetTrigger("Chill");
+    }
+}
