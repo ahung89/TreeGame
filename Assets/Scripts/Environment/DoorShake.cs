b7d793fd28734b1be22e02a6d998d561082b5a00
ackleyrc <ackleyrc@gmail.com>
2018-11-05 18:05:56 -0800

    Make Outside Door shake when trying to open

---
+++ Assets/Scripts/Environment/DoorShake.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorShake : MonoBehaviour
+{
+    public int shakeCount = 1;
+    public float shakeTime = 1f;
+    public float maxRotation = 5f;
+
+    private float shakeTimeRemaining = 0f;
+    private Vector3 defaultRotation;
+
+    private void Awake()
+    {
+        defaultRotation = transform.localRotation.eulerAngles;
+    }
+
+    private void Update()
+    {
+        if (shakeTimeRemaining > 0f)
+        {
+            float newXRotation = defaultRotation.x + Mathf.Sin(Mathf.PI * (shakeTime - shakeTimeRemaining) * (shakeCount / shakeTime)) * maxRotation;
+            this.transform.localRotation = Quaternion.Euler(newXRotation, defaultRotation.y, defaultRotation.z);
+            shakeTimeRemaining -= Time.deltaTime;
+        }
+    }
+
+    public void StartDoorShake()
+    {
+        shakeTimeRemaining = shakeTime;
+    }
+}
---
+++ Assets/Scripts/Environment/OutsideDoor.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OutsideDoor : Interactable
+{
+    AudioSource doorRattle;
+    DoorShake shakeEffect;
+
+    void Awake()
+    {
+        doorRattle = gameObject.GetComponent<AudioSource>();
+        shakeEffect = gameObject.GetComponent<DoorShake>();
+
+        Debug.Log("doorRattle: " + doorRattle);
+        Debug.Log("shakeEffect: " + shakeEffect);
+    }
+
+    public override bool CanInteractWith(Pickupable heldItem)
+    {
+        return !heldItem;
+    }
+
+    public override bool Interact(Pickupable heldItem)
+    {
+        if (!doorRattle.isPlaying)
+        {
+            doorRattle.Play();
+
+            if (shakeEffect)
+            {
+                shakeEffect.StartDoorShake();
+            }
+        }
+        return false;
+    }
+}
